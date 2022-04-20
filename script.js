9de891c097aeb94b46f96b266f8b8522becc3516
Daniel <flores.kd97@gmail.com>
2022-04-19 21:33:50 -0700

    created a function to store the value of button

---
+++ script.js
@@ -0,0 +1,33 @@
+const button = document.querySelector(".buttons");
+let op = document.querySelectorAll(".operator")
+let number = document.querySelectorAll(".numbers")
+
+function storeVar(el) {
+    let key = el.getAttribute('value');
+    if 
+}
+
+
+
+
+// function operate(operator, num1, num2) {
+    
+// }
+
+
+
+// function add(num1, num2) {
+//     return num1 + num2;
+// }
+
+// function subtract(num1, num2) {
+//     return num1 - num2;
+// }
+
+// function multiply(num1, num2) {
+//     return num1 * num2;
+// }
+
+// function divide(num1, num2) {
+//     return num1 / num2;
+// }
