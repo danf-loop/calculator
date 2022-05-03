864ed66627bfeb122dcb0d7b648b0026f1e219e6
Daniel <flores.kd97@gmail.com>
2022-05-03 13:29:35 -0700

    added checking previous key

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 // main calculator elements
+const calculator = document.querySelector('.grid')
 
-const keys = document.querySelector('.buttons')
+const keys = calculator.querySelector('.buttons')
 
-const display = document.querySelector('.result')
+const display = calculator.querySelector('.result')
+
+
+
+
 
 
 // store button value when clicked
@@ -12,28 +17,39 @@ keys.addEventListener('click', event => {
 
     const key = event.target;
     console.log(key);
+    
     const keyValue = key.textContent; // gets the actual value
     console.log(keyValue);
+    
     const displayValue = display.textContent; // the content in the display
+
+    const  { type } = key.dataset
+
+    const { previousKeyType } = calculator.dataset
 
 
     // Check if it a number key
-    if (key.classList.contains('numbers')) {
+    if (type == 'numbers') {
       if (displayValue == '0') { // the default display of 0 will be replaced with the value of the number clicked on otherwise join the numbers
         display.textContent = keyValue; // displays the pressed key onto the display
+    } else if (previousKeyType == 'operator') {
+        display.textContent = keyValue
     } else {
         display.textContent = displayValue + keyValue; // concatenating the number strings together
-    }
+        }
+ 
     }
    
 
     // Check if it is a operator key
-    if (key.classList.contains('operator')) {
+    if (type == 'operator') {
         console.log(key);
-    }
+      }
 
+    calculator.dataset.previousKeyType = type
 
-})
+    
+    })
 
 
 
@@ -52,25 +68,25 @@ keys.addEventListener('click', event => {
 
 
 
-function operate() {
+// function operate() {
 
     
-}
+// }
 
 
-function add(num1, num2) {
-    return num1 + num2;
-}
+// function add(num1, num2) {
+//     return num1 + num2;
+// }
 
-function subtract(num1, num2) {
-    return num1 - num2;
-}
+// function subtract(num1, num2) {
+//     return num1 - num2;
+// }
 
-function multiply(num1, num2) {
-    return num1 * num2;
-}
+// function multiply(num1, num2) {
+//     return num1 * num2;
+// }
 
-function divide(num1, num2) {
-    return num1 / num2;
-}
+// function divide(num1, num2) {
+//     return num1 / num2;
+// }
 
